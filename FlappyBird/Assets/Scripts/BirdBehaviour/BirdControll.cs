a72cbe2948df3bc1823fe2c293435ddba02d0444
Sergey Yakovlev <Mikrolab075@outlook.com>
2020-04-29 11:35:50 +0300

    Create player control system

--- FlappyBird/Assets/Scripts/BirdBehaviour/BirdControll.cs
+++ FlappyBird/Assets/Scripts/BirdBehaviour/BirdControll.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BirdControll : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerInputSystem _inputActions;
+    private Rigidbody2D _playerRigidbody;
+    [SerializeField] private float _jumpForce = 10;
+
+    private void Awake()
     {
-        
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+        _inputActions = new PlayerInputSystem();
+        _inputActions.BirdControll.KeyboardJump.performed += ctx => Jump();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Jump()
     {
-        
+        Debug.Log("hello");
+        _playerRigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.BirdControll.Enable();    
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.BirdControll.Disable();    
     }
 }
